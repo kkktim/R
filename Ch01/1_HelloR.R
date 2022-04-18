23c688bcad6466b7b2a7e9f3e08ab5d55d0058f9
unknown <rkdxogh1987@gmail.com>
2022-04-18 15:43:38 +0900

    2022/04/18 Ch01 수업예제

---
+++ Ch01/1_HelloR.R
@@ -0,0 +1,15 @@
+#날짜 : 2022/04/18
+#이름 : 강태호
+#내용 : Hello R
+
+print("Hello R!")
+print("안녕 R!")
+
+hello <- "Hello World"
+print(hello)
+
+welcome <- "welcome R!"
+welcome
+
+greeting <- 'Greeting R!'
+greeting
